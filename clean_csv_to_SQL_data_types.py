ff595a80f4be9c5cfc0bd64e2dad3a4ab994a323
A-Grossmann <70244924+A-Grossmann@users.noreply.github.com>
2022-12-02 15:04:26 -0500

    Update clean_csv_to_SQL_data_types.py

--- clean_csv_to_SQL_data_types.py
+++ clean_csv_to_SQL_data_types.py
@@ -2,6 +2,7 @@ import pandas as pd
 import csv
 
 #Opening the csv file
+#Edditing the file to not contain the Location in point(,) format for processing
 data = pd.read_csv("NYC_Accidents_2020.csv")
 pd.set_option('display.max_column',20)
 print(data.head())
