85dd2da1629828d73f0e45612a649f1db6e8bce9
A-Grossmann <70244924+A-Grossmann@users.noreply.github.com>
2022-12-09 10:21:22 -0500

    Update Step_by_step_py_sql_r.r
    This is all the work for the SQL python and R code.  The R code is the only part that does not have the comment (#) and can be ran directly from the document.

--- Step_by_step_py_sql_r.r
+++ Step_by_step_py_sql_r.r
@@ -2,21 +2,21 @@
 
     #In PYTHON
 
-        import pandas as pd
-        import csv
+#         import pandas as pd
+#         import csv
 
-        #Opening the csv file
-        #Edditing the file to not contain the Location in point(,) format for processing:
+#         #Opening the csv file
+#         #Edditing the file to not contain the Location in point(,) format for processing:
 
-        data = pd.read_csv("NYC_Accidents_2020.csv")
-        pd.set_option('display.max_column',20)
-        print(data.head())
+#         data = pd.read_csv("NYC_Accidents_2020.csv")
+#         pd.set_option('display.max_column',20)
+#         print(data.head())
 
-        print(data['CRASH DATE'])
+#         print(data['CRASH DATE'])
 
-        data.pop("LOCATION")
+#         data.pop("LOCATION")
 
-        data.to_csv("NYC_Accidents_2020_1.csv")
+#         data.to_csv("NYC_Accidents_2020_1.csv")
 
 #Migrate the csv file into mysql to better sort it:
 
